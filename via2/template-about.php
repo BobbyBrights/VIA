f0986bb29042a147971eaf65d49b3eb30d8956ae
Freddie Andrade <faandrade@gmail.com>
2015-09-12 18:42:15 -0400

    about page styles

--- via2/template-about.php
+++ via2/template-about.php
@@ -20,26 +20,26 @@ global $avia_config;
 			<div class='container' id="about">
 
 				<div class="two_col">
-					<div class="VIA_calendar">
-						<p class="VIA_calendar_title">Calendar</p>
-						<ul class="VIA_calendar_entries">
+					<div class="via_calendar">
+						<p class="via_calendar_header">Calendar</p>
+						<ul class="via_calendar_entries">
 						<?php
 
-							$VIA_calendar_args = array( 'post_type' => 'post', 'category_name' => 'calendar', 'posts_per_page' => -1 );
-							$VIA_calendar_posts = get_posts( $VIA_calendar_args );
+							$via_calendar_args = array( 'post_type' => 'post', 'category_name' => 'calendar', 'posts_per_page' => -1 );
+							$via_calendar_posts = get_posts( $via_calendar_args );
 
-							foreach ( $VIA_calendar_posts as $post ) : setup_postdata( $post );
+							foreach ( $via_calendar_posts as $post ) : setup_postdata( $post );
 
 								$event_date = new DateTime( get_field( 'event_date' ) );
 								$event_time = get_field( 'event_time' );
 								$event_location = get_field( 'event_location' );
 								$event_link = get_field( 'event_link' ); ?>
 							<li>
-								<span class="VIA_calendar_date"><?php echo $event_date->format('F j Y'); ?></span>
-								<span class="VIA_calendar_time"><?php echo $event_time; ?></span>
-								<span class="VIA_calendar_title"><?php the_title(); ?></span>
-								<span class="VIA_calendar_location"><?php echo $event_location; ?></span>
-								<span class="VIA_event_link"><?php echo $event_link; ?></span>
+								<span class="via_calendar_date"><?php echo $event_date->format('F j Y'); ?></span>
+								<span class="via_calendar_time"><?php echo $event_time; ?></span>
+								<span class="via_calendar_title"><?php the_title(); ?></span>
+								<span class="via_calendar_location"><?php echo $event_location; ?></span>
+								<span class="via_event_link"><?php echo $event_link; ?></span>
 							</li>
 						<?php endforeach; wp_reset_postdata(); ?>
 						</ul>
@@ -47,7 +47,22 @@ global $avia_config;
 					<?php echo get_field('VIA_description'); ?>
 				</div>
 
-				<main class='two_col template-page content  <?php avia_layout_class( 'content' ); ?> units' <?php avia_markup_helper(array('context' => 'content','post_type'=>'page'));?>>
+				<div class="one_col">
+					<div class="about_follow">
+						<strong>
+							VisionIntoArt <br />
+							25 Columbus Circle 68B <br />
+							NY, NY 10019
+						</strong>
+						<p>Telephone 646.942.7659 <br />info@visionintoart.org</p>
+						<p>Follow us:</p>
+						<ul class="social_icons">
+
+						</ul>
+					</div>
+					<?php echo get_field('board_members_description'); ?>
+				</div>
+				<main class='one_col template-page content  <?php avia_layout_class( 'content' ); ?> units' <?php avia_markup_helper(array('context' => 'content','post_type'=>'page'));?>>
 
                     <?php
                     /* Run the loop to output the posts.
